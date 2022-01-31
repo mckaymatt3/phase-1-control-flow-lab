93a2af5331f80285563232dea677d91faeaadc52
mckaymatt3 <mckaymatt@outlook.com>
2022-01-31 17:30:55 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  let result
+  if (someValue < 400) {
+    result = 'This one is on me!'
+  }
+  else if (someValue > 2500) {
+  result = 'No can do.'
+  }
+  else {
+    result = 'I will gladly take your thirty bucks.'
+  }
+  return result    
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(someCity){
   // Write your code here!
+  let result
+  if (someCity !== 'NYC') {
+    result='No go.'
+  }
+  else {
+    result='Ok, sounds good.'
+  }
+  return result
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(someTip){
   // Write your code here!
+  let result;
+  switch (someTip) {
+    case 'not as generous':
+      result = 'Thank you.';
+      break;
+    case 'generous':
+      result = 'Thank you so much.';
+      break;
+    default: 
+      result = 'Bye.'
+      break;
+  }
+  return result
 }
